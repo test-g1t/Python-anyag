f5d835794ee6665e1145e4893c1232e99ebecc36
szasz.roland <szaszroland11@gmail.com>
2021-12-02 19:19:15 +0100

    commit

--- Listák/lists1.py
+++ Listák/lists1.py
@@ -2,6 +2,7 @@
 numbers = [10, 5, 7, 2, 1]
 print("Ez az eredeti lista", numbers)
 
+# ---------------------------------------------------------------------------------------------
 
 newList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # az 1 ==> 0.-ik tag,
@@ -9,14 +10,39 @@ newList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # és így tovább
 # ez minden listában így van
 
+# ---------------------------------------------------------------------------------------------
+
+# addjunk a mostani listánkhoz egy újabb elemet
+numbers = [1, 2, 3, 4, 5, 6]
+print("Ez az eredeti listánk:", numbers)
 numbers[0] = 111
+print("Ez a változtatott listánk:", numbers)
+
 ###################################
 # numbers[0] = x
 # az x egy új lista tag ami lehet szám vagy szó is
 # és nem kicsréli a 0.-ik tagot hanem beszúrja a mostani elé
 ###################################
-print("Ez pedig a módosított lista", numbers)
 
+# ---------------------------------------------------------------------------------------------
+
+# Fűzzük a 6-os számot a lista VÉGÉRE
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+###################################
+# name.append(x)
+# name ==> a listád neve
+# x ==> a tag amit hozzá akarsz adni (ha stringet akarsz hozzádani akkor "" - ba kell tenni)
+# hozáfűzi a megadott karaktert a lista VÉGÉRE [<== FONTOS HOGY A VÉGÉRE TESZI]
+###################################
+
+# ---------------------------------------------------------------------------------------------
+
+# Írassuk ki a programunkal a listának a hosszát
+myList = [1, 2, "ListaElem", 20, 2.2]
+print("A lista", len(myList), "tag hosszú.")
 
 ###################################
 # len(x) függvény
@@ -24,7 +50,20 @@ print("Ez pedig a módosított lista", numbers)
 # megszámlálja a lista hosszát
 ###################################
 
+# ---------------------------------------------------------------------------------------------
 
-# Írassuk ki a programunkal a litának a hosszát
-myList = [1, 2, "ListaElem", 20, 2.2]
-print("A lista", len(myList), "tag hosszú.")
+# Töröljünk egy elemet a listánkból
+
+numbers = [1, 2, 3, 4, 5]
+print("Az eredeti listánk:", numbers)
+del numbers[1]
+print("A módosított listánk:", numbers)
+
+######################################################################
+# del name[x]
+# name ==> "a listád neve"
+# # ==> és, "hogy hányadik tagot akarod kitörölni"
+# !Figyelj a sorrendre, 0-val kezdődik a számolás!
+######################################################################
+
+# ---------------------------------------------------------------------------------------------
